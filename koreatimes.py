4a60dfa3321e3fe5da96b110c137886664cc78f8
kaustuv deolal <vutsuak96@gmail.com>
2016-02-13 11:44:08 +0530

    title and date for korea times

--- koreatimes.py
+++ koreatimes.py
@@ -3,7 +3,7 @@ __name__ = "vutsuak"
 import urllib2
 import BeautifulSoup as bs4
 
-html = urllib2.urlopen('http://www.inc.com/business-insider/google-hardest-interview-questions.html')
+html = urllib2.urlopen('http://www.koreatimes.co.kr/www/news/nation/2016/02/116_197716.html')
 
 soup = bs4.BeautifulSoup(html)
 content = ""
@@ -12,13 +12,11 @@ date = ""
 
 title = soup.title.string.split('|')[0]
 
-for i in soup.findAll('a'):
-    link = i.get('href')
-    if link is not None:
-        link = link.split('/')
-        if "author" in link:
-            author = link[-1]
-
+for i in soup.findAll('span'):
+    if i.string is not None:
+        if "Posted" in i.string.split(" "):
+            date= i.string
+'''
 for i in soup.findAll('h3'):
     content += str(i.string) + "\n"
 for i in soup.findAll('span'):
@@ -26,9 +24,9 @@ for i in soup.findAll('span'):
         k = i.string.split(" ")
         if "Published" in k:
             date = i.string
-
+'''
 print title
-print author
+#print author
 print date
-print content
+#print content
 # print videos
